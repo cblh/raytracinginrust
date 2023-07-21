87ba160fc6dda1a4d05f0de609707ab4f66f4e7a
burlinchen <chenbeiliuhuo@gmail.com>
2023-07-21 16:05:00 +0800

    feat: multi-sampled pixels

--- src/vec.rs
+++ src/vec.rs
@@ -51,14 +51,13 @@ impl Vec3 {
         self / self.length()
     }
 
-    pub fn format_color(self) -> String {
-        format!("{} {} {}", (255.999 * self[0]) as u64,
-        (255.999 * self[1]) as u64,
-        (255.999 * self[2]) as u64)
-        // let ir = 256.0 * (self[0] / (sample_per_pixel as f64)).clamp(0.0, 0.999);
-        // let ig = 256.0 * (self[1] / (sample_per_pixel as f64)).clamp(0.0, 0.999);
-        // let ib = 256.0 * (self[2] / (sample_per_pixel as f64)).clamp(0.0, 0.999);
-        // format!("{} {} {}", ir, ig, ib)
+    pub fn format_color(self, samples_per_pixel: u64) -> String {
+        let ir = (256.0 * (self[0] / (samples_per_pixel as f64)).clamp(0.0, 0.999)) as u64;
+        let ig = (256.0 * (self[1] / (samples_per_pixel as f64)).clamp(0.0, 0.999)) as u64;
+        let ib = (256.0 * (self[2] / (samples_per_pixel as f64)).clamp(0.0, 0.999)) as u64;
+    
+        format!("{} {} {}", ir, ig, ib)
+    
     }
 
 }
